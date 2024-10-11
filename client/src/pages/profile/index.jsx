135ef58305dbf3c5c1d2bbe16d3d4f16c347a7b1
srpraju <srpraju1554@gmail.com>
2024-10-11 23:34:12 +0530

    Profile UI

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,10 +1,77 @@
+import { useState } from "react";
 import { useAppStore } from "@/store"
+import { useNavigate } from "react-router-dom";
+import { IoArrowBack } from 'react-icons/io5'
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
+import { colors, getColor } from "@/lib/utils";
+import { FaTrash, FaPlus } from "react-icons/fa";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const Profile = () => {
-  const {userInfo} = useAppStore();
-  return <div>Profile
-    <div>Email: {userInfo.email}</div>
-  </div>
+  const navigate = useNavigate();
+  const { userInfo, setUserInfo } = useAppStore();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [image, setImage] = useState(null);
+  const [hovered, setHovered] = useState(false);
+  const [selectedColor, setSelectedColor] = useState(0);
+
+  const saveChanges = async () => { };
+
+  return (
+    <div className="bg-[#1b1c24] h-[100vh] flex items-center justify-center gap-10 flex-col">
+      <div className="flex flex-col gap-10 w-[80vw] md:w-max">
+        <div>
+          <IoArrowBack className="text-4xl lg:text-6xl text-white/90 cursor-pointer" />
+        </div>
+        <div className="grid grid-cols-2 ">
+          <div className="h-full w-32 md:w-48 md:h-48 relative flex items-center justify-center" onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}>
+            <Avatar className="h-32 w-32 md:w-48 md:h-48 rounded-full overflow-hidden">
+              {
+                image ? (<AvatarImage src={image} alt="profile" className="object-cover w-full h-full bg-black" />) :
+                  (
+                    <div className={`uppercase h-32 w-32 md:w-48 md:h-48 text-5xl border-[1px] flex items-center justify-center rounded-full ${getColor(selectedColor)}`}>
+                      {firstName ? firstName.split("").shift() : userInfo.email.split("").shift()}
+                    </div>
+                  )
+              }
+            </Avatar>
+            {
+              hovered && (<div className="absolute inset-0 flex justify-center items-center bg-black/50 ring-fuchsia-50 cursor-pointer rounded-full"> {image ? <FaTrash className="text-white cursor-pointer text-3xl" /> : <FaPlus className="text-white cursor-pointer text-3xl" />}</div>
+              )
+            }
+            {/* <input type="text" /> */}
+          </div>
+          <div className="flex min-w-32 md:min-w-64 flex-col gap-5 text-white items-center justify-center">
+            <div className="w-full">
+              <Input placeholder="Email" type="email" disabled value={userInfo.email} className="rounded-lg p-6 bg-[#2c2e3b] border-none" />
+            </div>
+
+            <div className="w-full">
+              <Input placeholder="First Name" type="text" onChange={(e) => setFirstName(e.target.value)} value={firstName} className="rounded-lg p-6 bg-[#2c2e3b] border-none" />
+            </div>
+
+            <div className="w-full">
+              <Input placeholder="Last Name" type="text" onChange={(e) => setLastName(e.target.value)} value={lastName} className="rounded-lg p-6 bg-[#2c2e3b] border-none" />
+            </div>
+
+            <div className="w-full flex gap-5">
+              {
+                colors.map((color, index) => (<div className={`${color} h-8 w-8 rounded-full cursor-pointer transition-all duration-300 ${selectedColor === index ? "outline outline-white/50 outline-1" : ""
+                  }`} key={index} onClick={() => setSelectedColor(index)}>
+                </div>
+                ))}
+            </div>
+          </div>
+        </div>
+        <div className="w-full">
+                <Button className="h-16 w-full bg-purple-700 hover:bg-purple-900 transition-all duration-300" onClick={saveChanges} >Save Changes</Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Profile
+export default Profile;
