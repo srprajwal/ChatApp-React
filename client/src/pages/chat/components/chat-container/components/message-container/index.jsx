2a294c1b05350aa4f5f7bc0d9709f16fd1d83211
srpraju <srpraju1554@gmail.com>
2024-10-15 23:32:18 +0530

    Chat Message Bar dane

---
+++ client/src/pages/chat/components/chat-container/components/message-container/index.jsx
@@ -0,0 +1,11 @@
+
+
+const MessageContainer = () => {
+  return (
+    <div className="flex-1 overflow-y-auto scrollbar-hidden p-4 md:w-[64vw] lg:w-[70vw] xl:w-[80vw] w-full">
+      
+    </div>
+  )
+}
+
+export default MessageContainer;
--- client/src/pages/chat/index.jsx
+++ client/src/pages/chat/index.jsx
@@ -2,6 +2,9 @@ import { useAppStore } from "@/store"
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
+import ChatContainer from "./components/chat-container";
+import ContactsContainer from "./components/contacts-container";
+import EmptyChatContainer from "./components/empty-chat-container";
 
 const Chat = () => {
 
@@ -16,8 +19,12 @@ const Chat = () => {
   }, [userInfo, nagative])
   
   return (
-    <div>
-      Chat
+    <div className="flex h-[100vh] text-white overflow-hidden">
+      <ContactsContainer/>
+      {/* <EmptyChatContainer/> */}
+      <ChatContainer/>
+      
+      
     </div>
   )
 }
