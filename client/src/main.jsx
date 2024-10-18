20c2e78b796815a94671ded35c3696354bc0ed4a
srpraju <srpraju1554@gmail.com>
2024-10-18 16:05:37 +0530

    Socket setup client done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,18 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Toaster } from "@/components/ui/sonner"
+import { SocketProvider } from './context/SocketContext.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
   <>
-    <App />
-    <Toaster closeButton />
+    <SocketProvider>
+
+      <App />
+      <Toaster closeButton />
+    </SocketProvider>
   </>
-    
+
   // </StrictMode>,
 )
